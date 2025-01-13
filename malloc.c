033f9c617ef59c6fd8c0967fcffe31b95bdf3301
vboxuser <vboxuser@linux20.myguest.virtualbox.org>
2025-01-13 15:49:16 +0530

    dyanmic allocation

---
+++ malloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define n 8
+
+int main(void)
+{
+    int *arr = malloc(n * sizeof(int));
+    if(arr == NULL)
+    {
+        perror("memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i =0 ;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+
+    }
+    for(int i =0 ;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
