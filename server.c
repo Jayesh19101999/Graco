80aa1d08db8b829a905f73cca7db9c537817ef6f
vboxuser <vboxuser@linux20.myguest.virtualbox.org>
2025-01-13 15:39:41 +0530

    unions

--- server.c
+++ server.c
@@ -1,67 +1,26 @@
-// server.c
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 
-#define PORT 8080
-#define MAX_BUFFER 1024
+// Define a typedef for the union
+typedef union {
+    int id;
+    char name[20];
+    float sensor;
+} Data;
 
-int main() {
-    int server_fd, new_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t addr_len = sizeof(client_addr);
-    char buffer[MAX_BUFFER] = {0};
+int main(void) {
+    // Declare a variable of the typedef-ed union
+    Data data1;
 
-    // Create a socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket failed");
-        exit(EXIT_FAILURE);
-    }
+    // Assign values to the union members
+    data1.id = 1;
+    printf("ID: %d\n", data1.id);
 
-    // Set the server address structure
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;  // Listen on all available interfaces
-    server_addr.sin_port = htons(PORT);
+    strcpy(data1.name, "pressure_switch");
+    printf("Name: %s\n", data1.name);
 
-    // Bind the socket to the address and port
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Bind failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
+    data1.sensor = 24.4;
+    printf("Sensor: %.2f\n", data1.sensor);
 
-    // Listen for incoming connections (maximum of 3 clients waiting in queue)
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port %d...\n", PORT);
-
-    // Accept incoming client connections
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, &addr_len)) < 0) {
-        perror("Accept failed");
-        close(server_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Client connected.\n");
-
-    // Read message from client
-    read(new_socket, buffer, MAX_BUFFER);
-    printf("Client: %s\n", buffer);
-
-    // Send a message to the client
-    const char *message = "Hello from server!";
-    send(new_socket, message, strlen(message), 0);
-    printf("Message sent to client: %s\n", message);
-
-    // Close the connection
-    close(new_socket);
-    close(server_fd);
     return 0;
 }
-
